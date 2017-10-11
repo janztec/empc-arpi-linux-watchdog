33d8642b58c9558cef70efc9e94bbecface63b5f
janztec-ama <andre.massow@janz.de>
2017-10-11 08:06:23 +0200

    Update install-experimental.sh

--- install-experimental.sh
+++ install-experimental.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+ERR='\033[0;31m'
+INFO='\033[0;32m'
+NC='\033[0m' # No Color
+
+if (cat /etc/issue | grep -v Raspbian) then
+    echo -e "$ERR ERROR: This script is only compatible with Raspbian Linux. $NC" 1>&2
+    exit 1
+fi
+
 echo "dtparam=watchdog=on" >>/boot/config.txt
 
 sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=14/g' /etc/systemd/system.conf
