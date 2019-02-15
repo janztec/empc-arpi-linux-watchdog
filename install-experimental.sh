00187910a34fbef6cc3a1ddf711283d772f90086
janztec-ama <14121872+janztec-ama@users.noreply.github.com>
2019-02-15 13:08:31 +0100

    Update install-experimental.sh

--- install-experimental.sh
+++ install-experimental.sh
@@ -4,12 +4,12 @@ ERR='\033[0;31m'
 INFO='\033[0;32m'
 NC='\033[0m' # No Color
 
-if [ $EUID -ne 0 ]; then
+if test "$EUID" -ne "0"; then
     echo -e "$ERR ERROR: This script should be run as root. $NC" 1>&2
     exit 1
 fi
 
-if (cat /etc/issue | grep -v Raspbian) then
+if cat /etc/issue | grep -v Raspbian; then
     echo -e "$ERR ERROR: This script is only compatible with Raspbian Linux. $NC" 1>&2
     exit 1
 fi
@@ -18,6 +18,6 @@ echo "dtparam=watchdog=on" >>/boot/config.txt
 
 sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=14/g' /etc/systemd/system.conf
 
-if (whiptail --title "Watchdog Installation Script" --yesno "Installation completed! reboot required\n\nreboot now?" 12 60) then
+if whiptail --title "Watchdog Installation Script" --yesno "Installation completed! reboot required\n\nreboot now?" 12 60; then
     reboot
 fi
